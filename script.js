c855268dfca8e2a2b81f2c60ec9ff383e2be5039
maggiomusic <maggiomusic@gmail.com>
2026-05-30 15:38:07 +0200

    Concept: testo intro sotto il titolo + ripristinati blocchi originali Missione/Visione/Ecosistemi/Open by design

--- script.js
+++ script.js
@@ -120,7 +120,7 @@ if (appsRail) {
 const revealEls = document.querySelectorAll(
     '.page__header, .home__center, .home__copyright, ' +
     '.node__stage, .node__detail, .node__cta, ' +
-    '.apps, .visit__image, .visit__info, .values__hero, .values__story, .values__block, .values__closing, .footer'
+    '.apps, .visit__image, .visit__info, .values__intro, .values__hero, .values__block, .values__closing, .footer'
 );
 revealEls.forEach(el => el.classList.add('reveal'));
 
